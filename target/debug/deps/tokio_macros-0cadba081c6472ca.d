43c4ffb680dd75a3c8cb908b3d3c0b8a1ca5c63d
synthalorian <synthalorian@github.com>
2026-06-05 09:58:09 -0400

    chore: add .gitignore, remove build artifacts

--- target/debug/deps/tokio_macros-0cadba081c6472ca.d
+++
@@ -1,7 +0,0 @@
-/home/synth/projects/sentryshark/target/debug/deps/tokio_macros-0cadba081c6472ca.d: /home/synth/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/lib.rs /home/synth/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/entry.rs /home/synth/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/select.rs
-
-/home/synth/projects/sentryshark/target/debug/deps/libtokio_macros-0cadba081c6472ca.so: /home/synth/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/lib.rs /home/synth/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/entry.rs /home/synth/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/select.rs
-
-/home/synth/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/lib.rs:
-/home/synth/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/entry.rs:
-/home/synth/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/select.rs:
