e4e2eed5e17dd286f10dfd8ad6c7600c56708a60
blackclaw <synthalorian@gmail.com>
2026-06-05 09:58:09 -0400

    chore: add .gitignore, remove build artifacts

--- target/debug/deps/sentryshark-a5821004b863c5e4.d
+++
@@ -1,5 +0,0 @@
-/home/synth/projects/sentryshark/target/debug/deps/sentryshark-a5821004b863c5e4.d: src/main.rs
-
-/home/synth/projects/sentryshark/target/debug/deps/sentryshark-a5821004b863c5e4: src/main.rs
-
-src/main.rs:
